a998b9394d6ca58f8e4c359691f3b1ef5bf8ef0d
Pogi <88253567+oPogi@users.noreply.github.com>
2025-03-12 01:48:44 -0400

    Update pTemplate.sql

--- pTemplate.sql
+++ pTemplate.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `esxlegacy_b4f3e3`
+-- Base de données : `pTemplate`
 --
 
 -- --------------------------------------------------------
